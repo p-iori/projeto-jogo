b13155c9a2bf6272f8ba548e0ffecd8c5d63ffc6
Pedro Iori <juxt.zt@proton.me>
2024-10-08 19:50:34 -0300

    Update no planetas.html (Tentando fazer o hover funcionar)

--- sistema1/planetas1.js
+++ sistema1/planetas1.js
@@ -1,3 +1,18 @@
+var coll = document.getElementsByClassName("menu-objetivos");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("ativo");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
 /*
 
 //Mostra o modal ao carregar a página
