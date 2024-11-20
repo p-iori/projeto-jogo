125d39c1d40e7e886123ee34d0a7784389595a6a
Pedro Iori <juxt.zt@proton.me>
2024-11-19 23:04:55 -0300

    Reformulação da história

--- sistema1/planetasIndex.js
+++ sistema1/planetasIndex.js
@@ -1,6 +1,25 @@
 var coll = document.getElementsByClassName("menu-objetivos");
 var i;
 
+//Confere as coordenadas
+document.getElementById("enter").addEventListener("click", confereCoord);
+
+function confereCoord(){
+    let coordInput = document.getElementById("coordenadas");
+    let coord = coordInput.value;
+
+    if(coord == ""){
+        alert("Preencha o campo antes de dar enter!");
+    }
+    if(coord == "314-mark-21"){
+        location.replace('./planetas/planeta1.html');
+        console.log("devia ter ido mas vc é burro");
+    } else if(coord !== "314-mark-21" && coord !== ""){
+        alert("Essas coordenadas não levam a lugar algum!");
+    }
+}
+
+//Mostra o menu de coordenadas
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("ativo");
@@ -13,11 +32,7 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
-/*
-
 //Mostra o modal ao carregar a página
 $(window).on('load', function () {
     $('#myModal').modal('show');
 });
-
-*/
