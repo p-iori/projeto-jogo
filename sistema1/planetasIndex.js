db7f61894dd576f9928da2124b37bb020b23c48a
Pedro Iori <juxt.zt@proton.me>
2024-11-20 21:32:47 -0300

    Planeta 1 começado

--- sistema1/planetasIndex.js
+++ sistema1/planetasIndex.js
@@ -1,11 +1,51 @@
-var coll = document.getElementsByClassName("menu-objetivos");
+const btnPlanetas = document.querySelectorAll('.planeta-ind');
+var coll = document.getElementsByClassName('menu-objetivos');
 var i;
 
+//Mostra o menu de coordenadas
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle("ativo");
+        var content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+}
+
+//Mostra o modal ao carregar a página
+$(window).on('load', function () {
+    $('#myModal').modal('show');
+});
+
+//Redireciona o usuário para o planeta escolhido
+ btnPlanetas.forEach(button => {
+    button.addEventListener('click', function(){
+     const numeroPlaneta = this.getAttribute('data-ind');
+
+     if(numeroPlaneta == 1){
+        location.replace('./planetas/planeta1.html');
+     } else if(numeroPlaneta == 2){
+        location.replace('./planetas/planeta2.html');
+     } else if(numeroPlaneta == 3){
+        location.replace('./planetas/planeta3.html');
+     } else if(numeroPlaneta == 4){
+        location.replace('./planetas/planeta4.html');
+     } else if(numeroPlaneta == 5){
+        location.replace('./planetas/planeta5.html');
+     } else {
+        console.log("alguma coisa deu muito errado")
+     }
+    })
+ })
+
 //Confere as coordenadas
-document.getElementById("enter").addEventListener("click", confereCoord);
+document.getElementById('enter').addEventListener('click', confereCoord);
 
 function confereCoord(){
-    let coordInput = document.getElementById("coordenadas");
+    let coordInput = document.getElementById('coordenadas');
     let coord = coordInput.value;
 
     if(coord == ""){
@@ -18,21 +58,3 @@ function confereCoord(){
         alert("Essas coordenadas não levam a lugar algum!");
     }
 }
-
-//Mostra o menu de coordenadas
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("ativo");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-
-//Mostra o modal ao carregar a página
-$(window).on('load', function () {
-    $('#myModal').modal('show');
-});
